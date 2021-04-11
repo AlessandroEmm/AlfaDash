fca34ce047a4450d471a9ae21c6ab00ca25fff25
Alessandro <alessandro.meyer@gmail.com>
2021-04-11 09:44:26 +0200

    oled

--- AlfaDash.ino
+++ AlfaDash.ino
@@ -1,26 +1,41 @@
 #include <SwitecX12.h>
+#include <OneBitDisplay.h>
 
 #define MAXRPM 7300
 #define MINRPM 500
 #define MINRPMPOSITION 3270
 
+OBDISP obd;
+#define SDA_PIN 43
+#define SCL_PIN 44
+// no reset pin needed
+#define RESET_PIN -1
+// let OneBitDisplay find the address of our display
+#define OLED_ADDR -1
+#define FLIP180 0
+#define INVERT 0
+// Use the default Wire library
+#define USE_HW_I2C 0
+
 const int STEPS = 315 * 12;
 const int A_STEP = 8;
 const int A_DIR = 9;
-const int RESET = 10;
+const int RESETPIN = 10;
+//HardwareSerial Serial3(PB_11, PB_10);
 
 SwitecX12 rpmmotor(STEPS, A_STEP, A_DIR);
 #define RX3_PIN 14
 #define TX3_PIN 15
 static uint32_t oldtime = millis();
+static uint32_t lastScreenUpdate = millis();
 uint8_t speedyResponse[100]; //The data buffer for the Serial data. This is longer than needed, just in case
 uint8_t byteNumber[2];  // pointer to which uint8_t number we are reading currently
 uint8_t readclt; // clt doesn't need to be updated very ofter so
 int clt;   // to store coolant temp
 unsigned int rpm;  //rpm and PW from speeduino
+unsigned int rpmBefore;
+int iat;
 float afr;
-float mapData;
-int8_t psi;
 float afrConv;
 uint8_t cmdAdata[40] ; 
 uint8_t test;
@@ -34,9 +49,20 @@ uint32_t sendTimestamp;
 
 long unsigned debug = millis();
 
-void setup() {
 
-  digitalWrite(RESET, HIGH);
+void setup() {
+  obdI2CInit(&obd, OLED_128x32, OLED_ADDR, FLIP180, INVERT, USE_HW_I2C, SDA_PIN, SCL_PIN, RESET_PIN, 400000L);
+  obdFill(&obd, 0, 0xff);
+  obdWriteString(&obd, 0, 0, 0,(char *)"RPM 0000", FONT_SMALL, 0, 1);
+  obdWriteString(&obd, 0, 56, 0,(char *)"IAT 0c ", FONT_SMALL, 0, 1);
+  obdWriteString(&obd, 0, 56, 1,(char *)"AFR 14.7", FONT_SMALL, 0, 1);
+  obdWriteString(&obd, 0, 0, 1,(char *)"Aqua 0c", FONT_SMALL, 0, 1);
+  obdWriteString(&obd, 0, 56, 2,(char *)"TPS 0%", FONT_SMALL, 0, 1);
+  obdWriteString(&obd, 0, 0, 2,(char *)"Adv 0", FONT_SMALL, 0, 1);
+  obdWriteString(&obd, 0, 0, 3,(char *)"Olio 0c, 0 Bar", FONT_SMALL, 0, 1);
+
+
+  digitalWrite(RESETPIN, HIGH);
   Serial.begin(9600);
   rpmmotor.zero();
   rpmmotor.setPosition(MINRPMPOSITION);
@@ -50,12 +76,21 @@ void loop() {
   if(received) {
     received = false;
   }
+
+if (isRPMDeviationBiggerThan(20))
+  {
     rpmmotor.setPosition(calcposition(rpm));
+   if (screenupdateDue()) {
+    updaterpm(rpm); 
+   }
+    rpmBefore = rpm;
+    }
   
   if (millis() > debug) {
-  debug = millis() + (long unsigned)500;
-  Serial.print("RPM-"); Serial.print(calcposition(rpm)); Serial.print("\n");
-  }
+    debug = millis() + (long unsigned)1000;
+    Serial.print("RPM-"); Serial.print(rpm); Serial.print("\n");
+   }
+    
 
 }
 
@@ -96,7 +131,22 @@ int calcposition(int currentRPM) {
 
 void processData() {
     rpm = ((speedyResponse [15] << 8) | (speedyResponse [14]));
+    //clt = (speedyResponse [15] << 8);
+    //iat = (speedyResponse [15] << 8);
+  }
 
+boolean isRPMDeviationBiggerThan(int number) {
+   int diff = abs((int)rpm - (int)rpmBefore);
+   return diff > number;
+}
+
+boolean screenupdateDue() {
+   Serial.print("RPM-"); Serial.print(millis() - lastScreenUpdate); Serial.print("\n");
+   return (millis() - lastScreenUpdate) > 5000;
+}
+
+
+void updaterpm(int rpm) {
 
   }
 
